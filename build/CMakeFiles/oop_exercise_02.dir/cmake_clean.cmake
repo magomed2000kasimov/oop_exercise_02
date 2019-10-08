622196485d0d62fe29d013f37e25c2dc90f4e0c1
magomed2000kasimov <magomed.kasimov.2000@mail.ru>
2019-10-08 08:26:22 +0300

    08.10.19

---
+++ build/CMakeFiles/oop_exercise_02.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oop_exercise_02.dir/main.cpp.o"
+  "CMakeFiles/oop_exercise_02.dir/bits.cpp.o"
+  "oop_exercise_02.pdb"
+  "oop_exercise_02"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oop_exercise_02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
